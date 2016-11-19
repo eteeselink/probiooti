ac2ff3de68eed605b0e1e390178a0a55e68ec9bf
Egbert Teeselink <skrebbel@gmail.com>
2016-11-19 22:24:48 +0100

    initial commit. wow this works well

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Forum2016
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using(var screen = new Screen()) {
+                screen.Clear();
+                screen.Write(20, 10, "    *   *   *     *   *   *    *   *   *     *   *   *");
+                screen.Write(20, 11, "      * * *         * * *        * * *         * * *  ");
+                screen.Write(20, 12, "    * * * * *     * * * * *    * * * * *     * * * * *");
+                screen.Write(20, 13, "      * * *         * * *        * * *         * * *  ");
+                screen.Write(20, 14, "    *   *   *     *   *   *    *   *   *     *   *   *");
+                Console.ReadKey(true);
+            }            
+        }
+    }
+}
---
+++ Screen.cs
@@ -0,0 +1,32 @@
+using System;
+namespace Forum2016
+{
+    class Screen : IDisposable
+    {
+        public void Write(int x, int y, string text, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.CursorLeft = x;
+            Console.CursorTop = y;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+
+            // somehow, CursorVisible = false doesn't seem to do much
+            // so we move the cursor to left top so it's a bit out of the way.
+            Console.CursorVisible = false;
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+        }
+
+        public void Clear()
+        {
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        public void Dispose()
+        {
+            Clear();
+        }
+    }
+
+}
