f07f6ea6f6614b9017b235f95fc9307ab403b0b9
Yimam <byimam@campus.tue.nl>
2016-11-24 17:30:16 +0100

    Berihun and Yongchao

---
+++ Antibiooti.Forum2016/Light.cs
@@ -0,0 +1,53 @@
+using System;
+
+namespace Antibiooti.Forum2016
+{
+    /// <summary>
+    /// Light to be displayed on the console with its status and color
+    /// </summary>
+    public class Light
+    {
+        private ConsoleColor _color;
+        public Boolean _status;
+
+
+        /// <summary>
+        /// Sets and Gets the Color of the Light Instance
+        /// </summary>
+        public ConsoleColor Color
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+            }
+        }
+        /// <summary>
+        /// Sets and Gets the Status of the Light Instance
+        /// </summary>
+        public Boolean Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+            }
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the Light class.
+        /// </summary>
+        public Light(ConsoleColor color, Boolean status)
+        {
+            this._color = color;
+            this._status = status;
+        }
+    }
+}
