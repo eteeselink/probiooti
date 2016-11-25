674c674edd441e7a259a9b4e4aeb0b8db87b2607
Mintchev <imintche@campus.tue.nl>
2016-11-25 10:31:14 +0100

    set character  method

--- Antibiooti.Forum2016/Character.cs
+++ Antibiooti.Forum2016/Character.cs
@@ -9,32 +9,34 @@ namespace Antibiooti.Forum2016
 
         public Character() {
             lights = new Light[]{
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
 
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
 
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
 
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
 
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true),
-                new Light(ConsoleColor.White,true)
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false),
+                new Light(ConsoleColor.White,false)
             };
             Clear();
         }
 
         public void Clear()
         {
-            // TODO turn all lights off
+            for (int i=0;i<17;i++){
+                lights[i].Status = false;
+            }
         }
 
         public void Write(int x, int y)
@@ -55,12 +57,64 @@ namespace Antibiooti.Forum2016
         public void SetCharacter(char c)
         {
             // To set the character
-            //
+            Clear();
+            switch (c)
+            {
+                case '.':
+                    lights[8].Status = true;
+                    break;
+                case 'o':
+                    lights[3].Status = true;
+                    lights[4].Status = true;
+                    lights[5].Status = true;
+                    lights[7].Status = true;
+                    lights[9].Status = true;
+                    lights[11].Status = true;
+                    lights[12].Status = true;
+                    lights[13].Status = true;
+                    break;
+                case 'O':
+                    lights[0].Status = true;
+                    lights[1].Status = true;
+                    lights[2].Status = true;
+                    lights[6].Status = true;
+                    lights[10].Status = true;
+                    lights[14].Status = true;
+                    lights[15].Status = true;
+                    lights[16].Status = true;
+                    break;
+                case '\\':
+                    lights[0].Status = true;
+                    lights[3].Status = true;
+                    lights[8].Status = true;
+                    lights[13].Status = true;
+                    lights[16].Status = true;
+                    break;
+                case '|':
+                    lights[1].Status = true;
+                    lights[4].Status = true;
+                    lights[8].Status = true;
+                    lights[12].Status = true;
+                    lights[15].Status = true;
+                    break;
+                case '/':
+                    lights[2].Status = true;
+                    lights[5].Status = true;
+                    lights[8].Status = true;
+                    lights[11].Status = true;
+                    lights[14].Status = true;
+                    break;
+                default:
+                    break;
+            }        
         }
 
         public void SetLightColor(int n, ConsoleColor color)
         {
             // To set the color of a specific light
+            for (int i=0;i<17;i++){
+                lights[i].Color = color;
+            }
         }
 
         public void SetLightOn(int n, int onoff)
